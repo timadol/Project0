80e685e550e4bc5d5d0116354b90b009c5be4b11
Timofey <timadol@yandex.ru>
2019-03-02 12:29:10 +0300

    Doesn't work at this time but big work was done

--- main.js
+++ main.js
@@ -1,108 +1,89 @@
 var mainTimer;
 
-var el;
+var canvasEl;
 var ctx;
-
-var circle0 = {};
-
-var circle = {};
-circle.x = 100;
-circle.y = 100;
-circle.vx = 10;
-circle.vy = 20;
-circle.radius = 40;
+var circles = {
+  arr: [],
+  draw: function(circle) {
+    ctx.beginPath();
+    ctx.fillStyle = "#000000";
+    ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI, true);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(
+      circle.x + circle.radius / 5,
+      circle.y - circle.radius / 5,
+      circle.radius / 5,
+      0,
+      2 * Math.PI,
+      true
+    );
+    ctx.fillStyle = "#808080";
+    ctx.fill();
+    ctx.fillStyle = "#000000";
+    ctx.closePath();
+  },
+  create: function(specs) {
+    var circle = Object(circles);
+    if (specs) {
+      circle.x = specs.x;
+      circle.y = specs.y;
+      circle.vx = specs.vx;
+      circle.vy = specs.vy;
+      circle.r = specs.r;
+    } else {
+      circle.r = 20;
+      // circle.x = circle.r + Math.random() * (ctx.width - 2 * circle.r);
+      // circle.y = circle.r + Math.random() * (ctx.width - 2 * circle.r);
+      circle.x = 100;
+      circle.y = 100;
+      circle.vx = Math.random() * 20;
+      circle.vy = Math.random() * 20;
+    }
+    this.arr.push(circle);
+  },
+  move: function(circle) {
+    if (circle.x + circle.r > ctx.width || circle.x - circle.r < 0) {
+      circle.vx = -circle.vx;
+    }
+    if (circle.y + circle.r > ctx.height || circle.x - circle.r < 0) {
+      circle.vy = -circle.vy;
+    }
+    circle.x += circle.vx;
+    circle.y += circle.vy;
+  }
+};
 
 function drawLoop() {
-  ctx.clearRect(0, 0, el.width, el.height);
-  
+  // canvasEl.style.background =
+  //   (canvasEl.style.background  != "black") ? "black" : "white";
+  ctx.fillStyle = "#ffffff";
+  ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
+  circles.arr.forEach(circle => {
+    circles.draw(circle);
+  });
+  circles.arr.forEach(circle => {
+    circles.move(circle);
+  });
 }
 
 function initJS() {
   mainTimer = setInterval(drawLoop, 1000);
 
-  el = document.getElementById("el");
-  ctx = el.getContext("2d");
-
-  circle0 = {
-    x: el.width / 2,
-    y: el.height / 2,
-    vx: 10,
-    vy: 20,
-    r: 40,
-    func: function(params) {
-      alert(params);
-    }
-  };
-
-  circle0.func("Hello");
-
-  alert();
+  canvasEl = document.getElementById("el");
+  ctx = canvasEl.getContext("2d");
+  circles.create();
+  // circle = {
+  //   x: canvasEl.width / 2,
+  //   y: canvasEl.height / 2,
+  //   vx: 10,
+  //   vy: 20,
+  //   r: 40,
+  //   move: function() {
+  //         earase(ctx, circle);
+  //         circle.x += circle.vx;
+  //         circle.y += circle.vy;
+  //         draw(ctx, circle);
+  //     }
+  // };
 }
-
-//   var example = document.getElementById("example");
-//   var ctx = example.getContext("2d");
-
-//   example.width = 640;
-//   example.height = 480;
-//   ctx.strokeRect(0, 0, 640, 480);
-
-//   function draw(ctx, circle) {
-//     ctx.beginPath();
-//     ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI, true);
-//     ctx.fill();
-//     ctx.beginPath();
-//     ctx.arc(
-//       circle.x + circle.radius / 5,
-//       circle.y - circle.radius / 5,
-//       circle.radius / 5,
-//       0,
-//       2 * Math.PI,
-//       true
-//     );
-//     ctx.fillStyle =#808080";
-//     ctx.fill();
-//     ctx.fillStyle =#000000";
-//   }
-//   function earase(ctx, circle) {
-//     ctx.beginPath();
-//     ctx.fillStyle =#ffffff";
-//     ctx.arc(circle.x, circle.y, circle.radius + 1, 0, 2 * Math.PI, true);
-//     ctx.fill();
-//     ctx.fillStyle =#000000";
-//   }
-/*while(true)
-        {
-            if((circle.x+circle.radius)>640 || (circle.x-circle.radius)<0)
-            {
-                circle.vx=-circle.vx;
-            }
-            if((circle.y+circle.radius)>480 || (circle.y-circle.radius)<0)
-            {
-                circle.vy=-circle.vy;
-            }
-            window.setInterval(function() {circle.x+=circle.vx;
-            circle.y+=circle.vy;},1000)
-            draw(ctx,circle);
-        }*/
-// function pausecomp(millis) {
-//     var date = new Date();
-//     var curDate = null;
-//     do { curDate = new Date(); }
-//     while (curDate - date < millis);
-// }
-// draw(ctx, circle);
-// function move() {
-//     earase(ctx, circle);
-//     circle.x += circle.vx;
-//     circle.y += circle.vy;
-//     draw(ctx, circle);
-// }
-// var counter = 5;
-// var startTime = new Date();
-
-//   while (true) {
-//   var stopTime = new Date();
-//   setTimeout(alert("time elapsed:" + +(stopTime - startTime)), 1000);
-// pausecomp(100);
-// alert("time elapsed:" + +(stopTime - startTime));
-//}
